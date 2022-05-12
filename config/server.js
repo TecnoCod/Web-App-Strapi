c6d5e2f0934a4f4447eb347f15baec0cfc8bd3ab
Christian Villegas <soliscvs@gmail.com>
2022-05-11 21:58:53 -0500

    01- strapi

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9bc48e13d69a04332d246a9c46a86251'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '08c51541-25c4-4a52-a029-d1ca36c18410'
+};
